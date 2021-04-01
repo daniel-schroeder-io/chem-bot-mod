54934bfc02de546745c6016d2c31d50cb95259da
Daniel Schroeder <me@danielschroeder.org>
2021-03-31 20:55:20 -0700

    waterbottle and shoutout

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+
+const { getLastStreamedGameByName } = require('./api');
 require('dotenv').config()
 const axios = require('axios');
 const tmi = require('tmi.js');
 
 let wincount = 0;
-
 const client = new tmi.Client({
 	options: { debug: true },
 	connection: { reconnect: true },
@@ -13,20 +14,55 @@ const client = new tmi.Client({
 	},
 	channels: [ process.env.CHANNEL ]
 });
-var minutes = 1;
-var the_interval = minutes * 60 * 1000;
+const shoutoutRegex = /(!so) (@[a-zA-Z]+)/g;
+
 
 client.connect();
 
-client.on('message', (channel, userstate, message, self) => {
-
-
+client.on('message', async (channel, userstate, message, self) => {
 
 	// Ignore echoed messages.
 	if(self) return;
 
+  //shoutout
+  if(message.match(shoutoutRegex))
+  {
+    var t = message.match(shoutoutRegex)
+    var name = t[0].split('@')[1];
+    let game = await getLastStreamedGameByName(name);
+    if (game[1] == null) {
+      client.say(channel,
+        `Make sure to check out Chem’s homie @${name}, ${game[0]}`
+        );
+    }
+    else {
+      client.say(channel,
+        `Make sure to check out Chem’s homie @${name}, they have been playing some ${game[1]}! ${game[0]}`
+        );
+    }
+  } 
+
+
+  if(message.toLowerCase() === '!pc' || message.toLowerCase() === '!specs')
+  {
+    client.say(channel, 
+      `AMD Ryzen 5 3600 |
+      MSI NVIDIA GTX 1660ti |
+      G Skillz Ripjaws V Series 3200 MHz DDR4 16 GB memory |
+      Samsung EVO 1 TB SSD  |
+      Seagate Barracuta 2 TB HDD |
+      Be Quiet! BK008 Pure Rock Slim CPU Cooler |
+      Corsair RM650x Power Supply`)
+  }
+
+  if(message.toLowerCase() === '!waterbottle')
+  {
+    client.say(channel, `Stone Brewing Hydroflask: https://shop.stonebrewing.com/hydro-flask-sbc-growler-40oz-003379 
+    With Stones Throw sticker: https://www.amazon.com/Stones-Throw-Logo-Sticker/dp/B00CNVJRNK `); 
+  }
+
   // If i get tagged, tell people what to do
-  if(message === `@${process.env.USERNAME}`) {
+  if(message.toLowerCase().includes(`@${process.env.USERNAME}`)) {
     client.say(channel, `Hey beautiful :)`);
   }
 
@@ -65,4 +101,5 @@ client.on('message', (channel, userstate, message, self) => {
       console.log(error);
     });
 	}
+ 
 });
